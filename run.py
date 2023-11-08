07c1bddb341c926c865f012a39e199edc06e92a2
Humanslaughter <theodorewikstrom@hotmail.com>
2023-11-08 21:58:11 +0000

    Add player and computer board

--- run.py
+++ run.py
@@ -9,4 +9,10 @@ print('')
 print('           Row: 1-8, Column: A-H')
 print('            Ships: 5, Turns: 10')
 print('=' * 42)
-print('')
+
+# Player and computer board
+player_board = [['_'] * 8 for x in range(8)]
+computer_board = [['_'] * 8 for x in range(8)]
+player_guess_board = [['_'] * 8 for x in range(8)]
+computer_guess_board = [['_'] * 8 for x in range(8)]
+
